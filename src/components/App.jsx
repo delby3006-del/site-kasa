2af278165a9aa5f32ac1b9cb65077864744ef279
delby3006-del <delby3006@gmail.com>
2026-02-12 10:32:03 +0100

    baniere header mise en page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,7 @@
 import "../styles/App.css";
-// import Banniere from "./Banniere.jsx";
-// import "../styles/banniere.css";
+import Banniere from "./Banniere.jsx";
 
 function App() {
-  return (
-    <div className="testclassestyle">
-      {/* <div className="header">
-        <Header imageUrl="/src/assets/images/LOGO.png" />
-      </div> */}
-      <h1>test reusi</h1>
-      {/* <Banniere
-        className="banniere-test"
-        titre="Kasa"
-        imageUrl="/src/assets/images/LOGO.png"
-      /> */}
-    </div>
-  );
+  return <Banniere />;
 }
 export default App;
--- src/components/Banniere.jsx
+++ src/components/Banniere.jsx
@@ -1,9 +1,18 @@
-import "./styles/banniere.css";
-export default function Banniere({ title }) {
+import "../styles/banniere.css";
+export default function Banniere() {
   return (
-    <section className="banniere" aria-label={title || "Banniere"}>
-      <div className="banniere-image" />
-      {title ? <h2 className="banniere-titre">{title}</h2> : null}
-    </section>
+    <header className="header">
+      <div className="logo-kasa">
+        <img src="/src/assets/images/LOGO.png" alt="Logo Kasa" />
+      </div>
+      <nav className="nav" aria-label="Navigation principale">
+        <a href="/" className="nav-link">
+          Accueil
+        </a>
+        <a href="/about" className="nav-link">
+          A Propos
+        </a>
+      </nav>
+    </header>
   );
 }
